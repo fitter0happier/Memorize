c039b1f3626740bb6a9aebab463f447eb48adce1
Tomchik, Roman <fitter0happier@protonmail.com>
2024-07-09 14:01:18 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Roman Tomchik on 09.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
